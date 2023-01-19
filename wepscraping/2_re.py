0ac715f0a7673ae7ef45a4db132cfdb9376bcba8
YoonDii <dbswl9506@naver.com>
2023-01-19 14:52:19 +0900

    search

--- wepscraping/2_re.py
+++ wepscraping/2_re.py
@@ -27,13 +27,22 @@ p = re.compile("ca.e")
 
 
 def print_match(m):
-
+    # if m:
+    #     print(m.group())
+    # else:
+    #     print("매칭되지 않음")
     if m:
-        print(m.group())
-    else:
-        print("매칭되지 않음")
+        print("m.group():", m.group())  # 일치하는 문자열 반환
+        print("m.string:", m.string)  # 입력받은 문자열
+        print("m.start():", m.start())  # 일치하는 문자열의 시작 index
+        print("m.end():", m.end())  # 일치하는 문자열의 끝 index
+        print("m.span()", m.span())  # 일치하는 문자열의 시작과 끝 index
 
 
 m = p.match("case")  # match : 주어진 문자열의 처음부터 일치하는지 확인
 print_match(m)
 # print(m.group())  # 위에 정규식과 매치가 되어서 출력됨./매치되지 않으면 에러가 발생
+
+
+m = p.search("good care")  # search : 주어진 문자열 중에 일치하는게 있는지 확인
+print_match(m)
