e94e8e0bfe827dcf7cfcc7ce4d32a784fd8a83b5
YoonDii <dbswl9506@naver.com>
2023-01-21 17:15:36 +0900

    엑셀파일만들어서 값저장하기

--- wepscraping/9_csv_stock.py
+++ wepscraping/9_csv_stock.py
@@ -1,10 +1,15 @@
-#액셀파일로 저장하는 연습
+#엑셀파일로 저장하는 연습
 
 import csv
 import requests
 from bs4 import BeautifulSoup
 
 url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
+
+filename = "시가총액1-200.csv"
+f = open(filename,"w",encoding="utf-8-sig", newline="") #공백을 넣는 이유는 줄바꿈을 없애기위해서
+# 엑셀파일에서 한글이 깨진다면 인코딩을 encoding="utf-8-sig" 이렇게 바꾸면 된다.
+writer = csv.writer(f)
 
 for page in range(1,5): # 4페이지까지 돌기
     res = requests.get(url + str(page))
@@ -17,4 +22,5 @@ for page in range(1,5): # 4페이지까지 돌기
         if len(colums) <= 1 : #의미없는 데이터는 skip
             continue
         date = [colum.get_text() for colum in colums]
-        print(date)
+        # print(date)
+        writer.writerow(date) #리스트형태로 넣어줘야함
