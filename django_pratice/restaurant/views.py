7f5e3a4a179efc96aa91d57cbdfaa37b79eb5e59
YoonDii <dbswl9506@naver.com>
2023-01-29 20:16:14 +0900

    views

--- django_pratice/restaurant/views.py
+++ django_pratice/restaurant/views.py
@@ -9,5 +9,9 @@ def index(request):
     return render(request,'restaurant/index.html',context=context)
 
 def food_detail(request,food):
-    context = {"name": food}
+    context = dict()
+    if food == "chicken":
+        context["name"] = "코딩에 빠진 닭"
+        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
+        context["price"] = 10000
     return render (request,'restaurnat/detail.html',context=context)
