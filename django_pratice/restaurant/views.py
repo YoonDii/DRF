e56692d5926a90eb3c2241ede8968f425c6559af
YoonDii <dbswl9506@naver.com>
2023-01-29 16:51:51 +0900

    views.py

--- django_pratice/restaurant/views.py
+++ django_pratice/restaurant/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime  import datetime
 # Create your views here.
 
 def index(request):
+    today = datetime.today()
+    print(today)
     return render(request,'restaurant/index.html')
