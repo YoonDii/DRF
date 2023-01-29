6da6e3752f2a8adcc5cbef9a5bc0ca658c61bb49
YoonDii <dbswl9506@naver.com>
2023-01-29 21:40:09 +0900

    get_static_prefix

--- django_pratice/restaurant/views.py
+++ django_pratice/restaurant/views.py
@@ -15,6 +15,7 @@ def food_detail(request,food):
         context["name"] = "코딩에 빠진 닭"
         context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
         context["price"] = 10000
+        context["img_path"] = 'restaurant/images/chicken.jpg'
     else:
-        raise Http404("이런 음식은 없다구요!")#404에러가 뜨게 해주는 코드
+        raise Http404("이런 음식은 없다구요!") #404에러가 뜨게 해주는 코드
     return render (request,'restaurant/detail.html',context=context)
