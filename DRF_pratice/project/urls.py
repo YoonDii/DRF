41f5187d19012911362b0a057fab024f85ef1e6c
YoonDii <dbswl9506@naver.com>
2023-01-30 14:17:05 +0900

    add urls.py

--- DRF_pratice/project/urls.py
+++ DRF_pratice/project/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import HelloAPI
+from .views import HelloAPI, bookAPI,booksAPI,BookAPI, BooksAPI #소문자book은 함수형, 대문자book은 클래스형
 
 urlpatterns = [
-    path("hello/", HelloAPI)
+    path("hello/", HelloAPI),
+    path('fbv/books/',booksAPI),
+    path('fbv/book/<int:bid>/',bookAPI),
 ]
