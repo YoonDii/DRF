4b8b510a57d40986d7072162da6a3306588b43d4
YoonDii <dbswl9506@naver.com>
2023-01-30 17:41:09 +0900

    생성

--- DRF_TODO/todo/serializers.py
+++ DRF_TODO/todo/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from .models import Todo
 
+#전체조회
 class TodosimpleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id','title','complete','important')
 
+#상세조회
 class TodoDetailSerializer(serializers.ModelSerializer):
     class Meta:
-        model:Todo
+        model=Todo
         fields = ('id','title','description','created','complete','important')
+
+#생성
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title','description','important')
--- DRF_TODO/todo/views.py
+++ DRF_TODO/todo/views.py
@@ -5,17 +5,24 @@ from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
 
 from .models import Todo
-from .serializers import TodosimpleSerializer,TodoDetailSerializer
+from .serializers import TodosimpleSerializer,TodoDetailSerializer,TodoCreateSerializer
 
 # Create your views here.
 
 class TodosAPIView(APIView):
-    def get(self,request):
+    def get(self,request):#전체조회
         todos = Todo.objects.filter(complete=False)
         serializer = TodosimpleSerializer(todos, many=True)
         return Response(serializer.data ,status=status.HTTP_200_OK)
 
-class TodoAPIView(APIView):
+    def post(self,request):#생성
+        serializer = TodoCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TodoAPIView(APIView):#상세조최
     def get(slef,request,pk):
         todo = get_object_or_404(Todo,id=pk)
         serializer = TodoDetailSerializer(todo)
