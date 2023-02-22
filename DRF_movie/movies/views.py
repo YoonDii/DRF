d5744c31e1cc4ff9f0f4c8a5791d1144969ee184
YoonDii <dbswl9506@naver.com>
2023-02-22 15:00:15 +0900

    add view

--- DRF_movie/movies/views.py
+++ DRF_movie/movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import api_view
+from rest_framework.response import Response
+
+from .models import Movie
+from .serializers import MovieSerializer
 
 # Create your views here.
+# @api_view(['GET'])으로 함수형 뷰인 movie_list()가 GET 메소드만 허용하는 API를 제공한다는 걸 표시
+@api_view(['GET'])
+def movies_list(request):
+    movies = Movie.objests.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data, status=200)
