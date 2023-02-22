74cbda2e5f7050e3c2945d1f6eafd60cc42614ea
YoonDii <dbswl9506@naver.com>
2023-02-22 16:17:25 +0900

    movie add post

--- DRF_movie/movies/serializers.py
+++ DRF_movie/movies/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from .models import Movie,Actor
 
 class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True) #조회만하고싶다
     name = serializers.CharField()
     opening_date = serializers.DateField()
     running_time = serializers.IntegerField()
     overview = serializers.CharField()
+
+    def create(self,validated_data):
+        return Movie.objects.create(**validated_data) # 유효성 검사를 마친 데이터
+
 
 class ActorSerializer(serializers.Serializer):
     id = serializers.IntegerField()
--- DRF_movie/movies/views.py
+++ DRF_movie/movies/views.py
@@ -1,16 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Movie , Actor
 from .serializers import MovieSerializer,ActorSerializer
 
 # Create your views here.
 # @api_view(['GET'])으로 함수형 뷰인 movie_list()가 GET 메소드만 허용하는 API를 제공한다는 걸 표시
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data, status=200)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        data = request.data 
+        serializer = MovieSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def actor_list(request):
