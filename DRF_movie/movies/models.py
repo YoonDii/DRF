716ea2640949846d0f9adc5718f91183accf8483
YoonDii <dbswl9506@naver.com>
2023-02-22 10:56:29 +0900

    add json

--- DRF_movie/movies/models.py
+++ DRF_movie/movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length =30)
+    opening_date = models.DateField()
+    running_time = models.IntegerField()
+    overview = models.TextField()
+
