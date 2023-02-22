d8e96de57a92fbd5f60fba0808e838d48bd3e137
YoonDii <dbswl9506@naver.com>
2023-02-22 18:30:09 +0900

    actor수정삭제

--- DRF_movie/movies/serializers.py
+++ DRF_movie/movies/serializers.py
@@ -13,7 +13,7 @@ class MovieSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name',instance.name)
-        instance.opening_date = validated_data.get('opening_date',instance.opening_data)
+        instance.opening_date = validated_data.get('opening_date',instance.opening_date)
         instance.running_time = validated_data.get('running_time',instance.running_time)
         instance.overview = validated_data.get('overview', instance.overview)
         instance.save()
@@ -28,3 +28,10 @@ class ActorSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name',instance.name)
+        instance.gender = validated_data.get('gender',instance.gender)
+        instance.birth_date = validated_data.get('birth_date',instance.birth_date)
+        instance.save()
+        return instance
