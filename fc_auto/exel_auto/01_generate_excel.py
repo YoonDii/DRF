56ecbd326fc6e37136a6fa9a39b9ed94d08afeb4
YoonDii <dbswl9506@naver.com>
2023-03-23 14:08:34 +0900

    담당자추가

--- fc_auto/exel_auto/01_generate_excel.py
+++ fc_auto/exel_auto/01_generate_excel.py
@@ -4,10 +4,12 @@ from openpyxl import Workbook
 class WeeklyWorkPlan:
     wb = None
     ws = None
+    manager = "매니저 이름을 입력 해주세요."
 
-    def __init__(self, sheet_no=0):  # sheet_no로 시트번호바꾸기
+    def __init__(self, manager, sheet_no=0):  # sheet_no로 시트번호바꾸기
         self.wb = Workbook()
         self.ws = self.wb.worksheets[sheet_no]
+        self.manager = manager
         self.set_title()
 
     def save(self, filename):
@@ -16,9 +18,9 @@ class WeeklyWorkPlan:
 
     def set_title(self):
         self.ws["B2"] = "담당자"
+        self.ws["C2"] = self.manager
 
 
 if __name__ == "__main__":
-    wwp = WeeklyWorkPlan()
-
+    wwp = WeeklyWorkPlan("yoondii")
     wwp.save("주간업무계획표.xlsx")
