90238f5a9d8fd2643b393e7daba7f46be9df3986
YoonDii <dbswl9506@naver.com>
2023-03-23 15:28:25 +0900

    시작일추가

--- fc_auto/exel_auto/01_generate_excel.py
+++ fc_auto/exel_auto/01_generate_excel.py
@@ -1,26 +1,33 @@
+# 라이브러리 불러오기
 from openpyxl import Workbook
 
-
+# 클래스만들고 함수정리하기
 class WeeklyWorkPlan:
-    wb = None
-    ws = None
+    wb = None  # 워크북
+    ws = None  # 워크시트
+    start_date = "2023-03-01"
     manager = "매니저 이름을 입력 해주세요."
 
-    def __init__(self, manager, sheet_no=0):  # sheet_no로 시트번호바꾸기
+    def __init__(self, start_date, manager, sheet_no=0):
         self.wb = Workbook()
-        self.ws = self.wb.worksheets[sheet_no]
+        self.ws = self.wb.worksheets[sheet_no]  # 어떤시트를 열것인지
+        self.start_date = start_date
         self.manager = manager
         self.set_title()
 
+    # 파일 저장하는 함수
     def save(self, filename):
         self.wb.save(filename)
         print("엑셀파일 생성 완료")
 
+    # 워크시트 셀선택과 입력값 설정
     def set_title(self):
         self.ws["B2"] = "담당자"
         self.ws["C2"] = self.manager
+        self.ws["B3"] = "시작일"
+        self.ws["C3"] = self.start_date
 
 
-if __name__ == "__main__":
-    wwp = WeeklyWorkPlan("yoondii")
+if __name__ == "__main__":  # 생성자 입력하기
+    wwp = WeeklyWorkPlan("2023-03-23", "yoondii")
     wwp.save("주간업무계획표.xlsx")
