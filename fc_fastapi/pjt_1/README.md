d924e9d688b4d3c6582a01ec0f94be16e861a014
YoonDii <dbswl9506@naver.com>
2023-03-23 11:29:36 +0900

    db연결

--- fc_fastapi/pjt_1/README.md
+++ fc_fastapi/pjt_1/README.md
@@ -1,1 +1,4 @@
-# 
+### DBeaver을 사용해서 데이터베이스 만들기
+https://dbeaver.io/download/
+자신에게 맞는 버전 설치하기
+
--- fc_fastapi/pjt_1/config.py
+++ fc_fastapi/pjt_1/config.py
@@ -8,12 +8,13 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     BASE_DIR: str = path.dirname((path.abspath(__file__)))
+    #개발은 다윈에서(맥os)또는 windows에서. 배포는 리눅스에서 할 예정.로컬모드설정
     LOCAL_MODE: bool = True if system().lower().startswith("darwin") or system().lower().startswith("Windows") else False
     app_name: str = "Imizi API"
     TEST_MODE: bool = False
 
-    ALLOW_SITE = ["*"]
-    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"] #어떤 프론트엔드를 허용할것인지.
+    TRUSTED_HOSTS = ["*"] 
     JWT_ALGORITHM = "HS256"
     JWT_SECRET_KEY = os.getenv("JWT_SECRET", "imizi-secret")
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 1  # one day
@@ -26,19 +27,19 @@ class Settings(BaseSettings):
 
 
 class DevSettings(Settings):
-    DB_URL = "mysql+pymysql://root:root@localhost:3306/imizi?charset=utf8mb4"
+    DB_URL = "mysql+pymysql://imizi_app:imizi_app_dev1@localhost:3306/imizi?charset=utf8mb4"
     DB_POOL_SIZE = 5
     DB_MAX_OVERFLOW = 10
 
 
 class TestSettings(Settings):
-    DB_URL = "mysql+pymysql://root:root@localhost:3306/imizi?charset=utf8mb4"
+    DB_URL = "mysql+pymysql://imizi_app:imizi_app_dev1@localhost:3306/imizi?charset=utf8mb4"
     DB_POOL_SIZE = 1
     DB_MAX_OVERFLOW = 0
 
 
 class ProdSettings(Settings):
-    DB_URL = "mysql+pymysql://root:root@localhost:3306/imizi?charset=utf8mb4"
+    DB_URL = "mysql+pymysql://imizi_app:imizi_app_dev1@localhost:3306/imizi?charset=utf8mb4"
     DB_POOL_SIZE = 5
     DB_MAX_OVERFLOW = 10
 
