5a31f4bc6561f1e500b03d3ec0ed4174543b2a90
YoonDii <dbswl9506@naver.com>
2023-03-23 09:57:39 +0900

    uvicorn

--- fc_fastapi/main.py
+++ fc_fastapi/main.py
@@ -12,3 +12,8 @@ def index():
 @app.get("/math/sum")
 def math_sum(number_1: int, number_2: int):
     return {"reuslt": number_1 + number_2}
+
+
+@app.get("/math/mul")
+def math_mul(number_1: int, number_2: int = 3):  # number_2는 defult값으로 3을 지정
+    return {"result": number_1 * number_2}
