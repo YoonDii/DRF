c2c4c4a42521539665e1db2aa45a046f9ac4b340
YoonDii <dbswl9506@naver.com>
2023-03-25 14:55:57 +0900

    add question router

---
+++ jumpto_fastapi/domain/question/question_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from database import SessionLocal
+from models import Question
+
+router = APIRouter(
+    prefix="/api/question",
+)
+#라우팅이란 FastAPI가 요청받은 URL을 해석하여 그에 맞는 함수를 실행하여 그 결과를 리턴하는 행위
+@router.get("/list")
+def question_list():
+    db = SessionLocal()
+    _question_list = db.query(Question).order_by(Question.creates.desc()).all()
+    db.close() # 사용한 세션을 컨넥션 풀에 반환하는 함수
+    return _question_list
--- jumpto_fastapi/main.py
+++ jumpto_fastapi/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from domain.question import question_router
+
 app = FastAPI()
 
-@app.get("/Hello")
-def hello():
-    return {"message": "안녕하세요 주말입니다"}
+# @app.get("/Hello")
+# def hello():
+#     return {"message": "안녕하세요 주말입니다"}
+
+app.include_router(question_router.router)
