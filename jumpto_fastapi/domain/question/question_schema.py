7dde06fe718da6957a06cfd2a9febc94e87764db
YoonDii <dbswl9506@naver.com>
2023-03-26 12:58:30 +0900

    add answer

--- jumpto_fastapi/domain/question/question_schema.py
+++ jumpto_fastapi/domain/question/question_schema.py
@@ -2,11 +2,14 @@ import datetime
 
 from pydantic import BaseModel
 
+from domain.answer.answer_schema import Answer
+
 class Question(BaseModel):
     id : int
     subject : str
     content : str
     creates: datetime.datetime
+    answers : list[Answer] = []
     #4가지 항목 모두 디폴트값이 없기때문에 필수항목이다.
 
     class Config:
--- jumpto_fastapi/main.py
+++ jumpto_fastapi/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from domain.question import question_router
+from domain.answer import answer_router
 
 app = FastAPI()
 
@@ -9,3 +10,4 @@ app = FastAPI()
 #     return {"message": "안녕하세요 주말입니다"}
 
 app.include_router(question_router.router)
+app.include_router(answer_router.router)
